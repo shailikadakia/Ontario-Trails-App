13cde140c4a4fd957d0e769e5834551868307004
Allen Liu <a256liu@uwaterloo.ca>
2021-06-25 11:51:54 -0400

    Use json file instead of geojson for data

--- rest-api/src/index.js
+++ rest-api/src/index.js
@@ -12,7 +12,7 @@ const API_V1_ROOT = "/api/v1";
 
 app.get(`${API_V1_ROOT}/trails/:id`, (req, res) => {
     const id = parseInt(req.params.id);
-    const selection = data.features.find(trail => trail.properties.OGF_ID === id);
+    const selection = data.find(trail => trail.OGF_ID === id);
     if (selection) {
         return res.json(selection).status(200);
     }
@@ -21,18 +21,15 @@ app.get(`${API_V1_ROOT}/trails/:id`, (req, res) => {
 
 
 app.get(`${API_V1_ROOT}/trails`, (_, res) => {
-    return res.json(data.features).status(200);
+    return res.json(data).status(200);
 });
 
 
 // Does not persist across multiple runs
 app.post(`${API_V1_ROOT}/trails`, (req, res) => {
-    const trail = {
-        type: "Feature",
-        properties: req.body,
-    };
-    trail.properties.OGF_ID = parseInt(trail.properties.OGF_ID);
-    data.features.push(trail);
+    const trail = req.body;
+    trail.OGF_ID = parseInt(trail.OGF_ID);
+    data.push(trail);
     return res.json(trail).status(201);
 });
 
