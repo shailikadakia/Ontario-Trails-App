162668bd02c032ec6215cf23341da01e3851571f
Allen Liu <a256liu@uwaterloo.ca>
2021-06-25 12:13:52 -0400

    Added CORS to Rest API

--- rest-api/src/index.js
+++ rest-api/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import data from "../data";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 
 const API_V1_ROOT = "/api/v1";
