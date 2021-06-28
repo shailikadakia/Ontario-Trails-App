175ce8262c47ea81697344a8addd4e4499c56527
Labiba <labiba@Labibas-MacBook-Pro.local>
2021-06-28 10:02:32 -0400

    fixed the field name in trail.js

--- react-app/src/components/Trail.js
+++ react-app/src/components/Trail.js
@@ -10,7 +10,7 @@ export const Trail = () => {
         {title: 'Name', field: 'TRAIL_NAME'},
         {title: 'Association', field: 'TRAIL_ASSOCIATION'},
         {title: 'Activity', field: 'ACTIVITY'},
-         {title: 'Length in KM', field: 'Length_KMS'},
+         {title: 'Length in KM', field: 'LENGTH_KMS'},
         //{title: 'Environmental Concerns', field: 'ENVIRONMENTAL_CONCERNS'},
     ]
     // This *should* work, but may break
